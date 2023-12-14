b896069fa672732be49ec86d01b279d5abc26135
skalskibukowa <bartosz.markiewicz01@gmail.com>
2023-12-15 00:30:55 +0100

    Added new methods in InventoryService updateProductQuantity / getProductById / removeProduct / getAllProducts

--- inventory/src/main/java/com/bartoszmarkiewicz/inventory/InventoryService.java
+++ inventory/src/main/java/com/bartoszmarkiewicz/inventory/InventoryService.java
@@ -1,17 +1,21 @@
 package com.bartoszmarkiewicz.inventory;
 
 
+import com.bartoszmarkiewicz.inventory.exceptions.InventoryNotFoundException;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
-import java.time.LocalDateTime;
 
+import java.util.List;
+import java.util.Optional;
+
+@Slf4j
 @Service
 @AllArgsConstructor
 public class InventoryService {
 
     private final InventoryRepository inventoryRepository;
-
 
     // AddProduct method to inventory
     public void addProduct(InventoryProductAddRequest inventoryProductAddRequest) {
@@ -24,6 +28,58 @@ public class InventoryService {
                 .build();
 
         inventoryRepository.saveAndFlush(inventory);
+    }
+
+    // GetProducts
+    public List<Inventory> getAllProducts() {
+        try {
+            return inventoryRepository.findAll();
+        } catch (Exception e) {
+            log.error("The list of products don't exist");
+            return null;
+        }
+    }
+
+
+    // Retrieve specific product by productId
+    public Optional<Inventory> getProductById(Integer productId) {
+        return Optional.ofNullable(inventoryRepository.findById(productId).orElseThrow(() -> new InventoryNotFoundException(productId)));
+    }
+
+    // update product quantity
+
+
+    public void updateProductQuantity(Integer productId, Long newQuantity) {
+
+        try {
+            Inventory inventory = inventoryRepository.findById(productId)
+                    .orElseThrow(() -> new InventoryNotFoundException(productId));
+
+            // Condition to not allow quantity update by negative value
+            if (newQuantity < 0) {
+                throw new IllegalArgumentException("Product quantity cannot be updated with a negative value: " + newQuantity);
+
+                // log.error("Product quantity cannot be updated with negative value: {}", newQuantity);
+                // return;
+            }
+
+            inventory.setProductQuantity(newQuantity);
+            inventoryRepository.saveAndFlush(inventory);
+        } catch (InventoryNotFoundException e) {
+            // Exception
+            log.error("Failed to update product quantity: Product not found", e);
+        }
+
+    }
+
+    // remove Product by ProductId
+    public void removeProduct(Integer productId) {
+        try {
+            inventoryRepository.deleteById(productId);
+        } catch (InventoryNotFoundException e) {
+            //Exception
+            log.error("Failed to remove product: Product not found", e);
+        }
     }
 
 
