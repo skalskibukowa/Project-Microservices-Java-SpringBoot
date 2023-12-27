be4ba047a10725171a2f931bca1a6783b7abf3e0
skalskibukowa <bartosz.markiewicz01@gmail.com>
2023-12-27 22:35:40 +0100

    Configure OpenFeign - created new module clients that store fraud interface. Record FraudCheckResponse has been moved to the clients module in the OrderApplication has been enabled OpenFeign clients.

--- order/src/main/java/com/bartoszmarkiewicz/order/OrderApplication.java
+++ order/src/main/java/com/bartoszmarkiewicz/order/OrderApplication.java
@@ -4,9 +4,13 @@ package com.bartoszmarkiewicz.order;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableEurekaClient
+@EnableFeignClients(
+        basePackages = "com.bartoszmarkiewicz.clients"
+)
 public class OrderApplication {
     public static void main(String[] args) {
         SpringApplication.run(OrderApplication.class, args);
